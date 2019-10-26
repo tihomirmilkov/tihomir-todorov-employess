5603930d44ee494cf4bf9d977cb48780b9c420ed
Tihomir Milkov Todorov <50403948+tihomirmilkov@users.noreply.github.com>
2019-10-26 15:06:06 +0300

    Remove unused files. Front end looks reasonable. Grid not added yet though. Finally buttons work and file upload is connected to Controller handler.

--- tihomir-todorov-employess/Controllers/HomeController.cs
+++ tihomir-todorov-employess/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using tihomir_todorov_employess.Models;
 
@@ -15,29 +17,39 @@ namespace tihomir_todorov_employess.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFileAsync(FileModel file)
+        {
+            if (file.FileUpload.FileData == null || file.FileUpload.FileData.Length <= 0)
+            {
+                return BadRequest(); // 400
+            }
+
+            try
+            {
+                // download file in temp location
+                var filePath = Path.GetTempFileName();
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.FileUpload.FileData.CopyToAsync(stream);
+                }
+
+                // process uploaded file
+                // Don't rely on or trust the FileName property without validation.
+
+            }
+            catch (Exception ex)
+            {
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError); // 500
+            }
+
+            return Ok(); // 200
         }
     }
 }
---
+++ tihomir-todorov-employess/Models/FileModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace tihomir_todorov_employess.Models
+{
+    public class FileModel : PageModel
+    {
+        [BindProperty]
+        public SingleFileUpload FileUpload { get; set; }
+    }
+
+    public class SingleFileUpload
+    {
+        [Required]
+        [Display(Name = "File")]
+        public IFormFile FileData { get; set; }
+    }
+}
